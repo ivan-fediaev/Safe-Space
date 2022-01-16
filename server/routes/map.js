4240b51848b73033537496b1790e5a8445d18a38
sanjeev <sanjeevk1999@gmail.com>
2022-01-15 17:55:52 -0800

    serverroutes

--- server/routes/map.js
+++ server/routes/map.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const Map = require('../models/map');
 
 // Get all markers
 router.get('/', (req, res) => {
-    
+    try {
+        const maps = await Map.find();
+        res.json(maps);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
 });
 
 // Get a single marker
@@ -13,7 +19,21 @@ router.get('/:id', (req, res) => {
 
 // Create a marker
 router.post('/', (req, res) => {
-
+    const map = new Map({
+        title: req.body.title,
+        address1: req.body.address1,
+        address2: req.body.address2,
+        coords: req.body.coords,
+        placeId: req.body.placeId,
+        upvotes: req.body.upvotes,
+        downvotes: req.body.downvotes,
+    });
+    try {
+        const newMap = await map.save();
+        res.status(201).json(newMap);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
 });
 
 // Update a marker
