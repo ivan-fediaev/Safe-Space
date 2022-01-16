d05a245c1dff2b9dc7d76da8c7dea8108c534992
fediaev <ivanfediaev@hotmail.com>
2022-01-15 22:45:12 -0800

    deleted some files

--- src/locatorplus.js
+++ src/locatorplus.js
@@ -37,47 +37,6 @@ function LocatorPlus(configuration) {
 
   // Initialize the map -------------------------------------------------------
   locator.map = new google.maps.Map(mapEl, configuration.mapOptions);
-  let infoWindow = new google.maps.InfoWindow()
-
-  const locationButton = document.createElement("button");
-  locationButton.textContent = "Pan to Current Location";
-  locationButton.classList.add("custom-map-control-button");
-
-  locator.map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
-  locationButton.addEventListener("click", () => {
-    // Try HTML5 geolocation.
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          const pos = {
-            lat: position.coords.latitude,
-            lng: position.coords.longitude,
-          };
-
-          infoWindow.setPosition(pos);
-          infoWindow.setContent("Location found.");
-          infoWindow.open(map);
-          map.setCenter(pos);
-        },
-        () => {
-          handleLocationError(true, infoWindow, locator.map.getCenter());
-        }
-      );
-    } else {
-      // Browser doesn't support Geolocation
-      handleLocationError(false, infoWindow, locator.map.getCenter());
-    }
-  });
-
-  function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-    infoWindow.setPosition(pos);
-    infoWindow.setContent(
-      browserHasGeolocation
-        ? "Error: The Geolocation service failed."
-        : "Error: Your browser doesn't support geolocation."
-    );
-    infoWindow.open(map);
-  }
 
   // Store selection.
   const selectResultItem = function(locationIdx, panToMarker, scrollToResult) {
